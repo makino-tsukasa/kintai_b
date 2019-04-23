bb4d1c08845dea62c8446aa21cbf479950ff49ec
MakinoTsukasa <aoitori.toyohashi@gmail.com>
2019-04-23 06:49:40 +0000

    Finish static page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  #application.html.erbの<head><title>に入る  
+  def full_title(page_title = "")
+    base_title = "勤怠管理システム"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+
 end
