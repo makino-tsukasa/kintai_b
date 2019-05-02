4536bbe08c65822f21ec81b8c60f904282c56b44
MakinoTsukasa <aoitori.toyohashi@gmail.com>
2019-05-02 07:02:09 +0000

    Finish users-signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-
-  # get 'users/new' ※rails g controller Users newで自動生成されたもの、下記に書き換え
-  get '/signup', to: 'users#new'
+  get  '/signup',  to: 'users#new'
+  resources :users
 end
